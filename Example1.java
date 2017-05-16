fd52817cff54598b26e95d37e0cd39dff4ad9b8f
Rupesh <bharuka.rupesh@gmail.com>
2017-05-16 18:12:53 +0530

    Example1 and README files are modified

---
+++ Example1.java
@@ -0,0 +1,40 @@
+package java8newfeatures;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
+/*
+ * Java 8 foreach loop example
+ * 
+*/
+public class Example1 {
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();
+//		
+//		list.add("Rupesh");
+//		list.add("Amit");
+//		list.add("Shekhar");
+//		list.add("Shubham");
+//		list.add("Aakash");
+//		
+//		
+//		list.forEach(name->{
+//			
+//			System.out.println(name);
+//			
+//			if("Rupesh".equalsIgnoreCase(name))
+//				System.out.println("\"" +name +"\"" +" is available in list");
+//			
+//		});
+//		
+//		
+		// no need of specifying parameter 
+		// it will automatically infer
+//		System.out.println("Shortest way...!");
+//		list.forEach(System.out::println);
+		
+		
+		  
+	}
+}
